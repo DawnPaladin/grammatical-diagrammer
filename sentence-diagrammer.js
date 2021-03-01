dc93c9d86d47568170c2d460bb01f65ab1771e29
James Harris <james.harris@cdbr.org>
2021-03-01 14:40:36 -0600

    Adjectives

--- sentence-diagrammer.js
+++ sentence-diagrammer.js
@@ -16,3 +16,18 @@ var jumps = draw.text("jumps").move(250+125, 70).font(textFont);
 
 var nounLabel = draw.text("(noun)").move(150, 70).font(labelFont).addClass('label');
 var verbLabel = draw.text("(verb)").move(410, 70).font(labelFont).addClass('label');
+
+function addModifier(xYcoords, wordStr, labelStr) {
+	const [xStart, yStart] = xYcoords;
+	const lineLength = 200;
+	var line = draw.line(xStart, yStart, xStart+lineLength, yStart).stroke(lineStyle);
+	var word = draw.text(wordStr).move(xStart+50, yStart-30).font(textFont);
+	var labelOffset = wordStr.length * 8 + 50;
+	var label = draw.text("("+labelStr+")").move(xStart+labelOffset, yStart-30).font(labelFont).addClass('label');
+	var group = draw.group();
+	group.add(word).add(label).add(line);
+	group.transform({ rotate: 63, origin: "bottom left", translateX: 40 })
+}
+addModifier([10, 100], "the", "article");
+addModifier([60, 100], "quick", "adjective");
+addModifier([110, 100], "brown", "adjective");
